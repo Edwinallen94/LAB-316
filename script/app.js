196acf62df9465859e666098b39ab840a85fa20e
Edwin <Edwinallen94@aol.com>
2024-09-20 14:35:39 -0400

    created the Submenu

--- script/app.js
+++ script/app.js
@@ -40,3 +40,12 @@ menuLinks.forEach((link) => {
 });
 
 //part 2
+
+// Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl.
+let subMenuEl = document.getElementById("sub-menu");
+// Set the height subMenuEl element to be "100%".
+subMenuEl.style.height = "100%";
+// Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property.
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+// Add the class of flex-around to the subMenuEl element.
+subMenuEl.classList.add("flex-around");
